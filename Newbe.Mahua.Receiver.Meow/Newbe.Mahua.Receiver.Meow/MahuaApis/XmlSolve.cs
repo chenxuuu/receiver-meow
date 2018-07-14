2e749295b67130b28bf44dcdf05b89e0111126b6
chenxuuu <lolicon@papapoi.com>
2018-07-14 12:43:04 +0800

    增加xml数据操作库

---
+++ Newbe.Mahua.Receiver.Meow/Newbe.Mahua.Receiver.Meow/MahuaApis/XmlSolve.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Newbe.Mahua.Receiver.Meow.MahuaApis
+{
+    class XmlSolve
+    {
+        static string path = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "data/";//AppDomain.CurrentDomain.SetupInformation.ApplicationBase
+
+        public static string ReplayGroupStatic(long fromGroup, string msg)
+        {
+            string replay_ok = replay_get(fromGroup, msg);
+            string replay_common = replay_get(2333, msg);
+
+            //CQ.SendGroupMessage(fromGroup, msg);
+
+            //System.Windows.Forms.MessageBox.Show(msg);
+
+            if (replay_ok != "")
+            {
+                if (replay_common != "")
+                {
+                    Random ran = new Random(System.DateTime.Now.Millisecond);
+                    int RandKey = ran.Next(0, 2);
+                    if (RandKey == 0) { return replay_ok; } else { return replay_common; }
+                }
+                else
+                {
+                    return replay_ok;
+                }
+            }
+            else if (replay_common != "")
+            {
+                return replay_common;
+            }
+            return "";
+        }
+
+        public static string replay_get(long group, string msg)
+        {
+            dircheck(group);
+            XElement root = XElement.Load(path + group + ".xml");
+            string[] replay_str = new string[50];
+            int count = 0;
+            Random ran = new Random(System.DateTime.Now.Millisecond);
+            int RandKey;
+            string ansall = "";
+            foreach (XElement mm in root.Elements("msginfo"))
+            {
+                if (msg.IndexOf(mm.Element("msg").Value) != -1 && count < 50)
+                {
+                    replay_str[count] = mm.Element("ans").Value;
+                    count++;
+                }
+            }
+            if (count != 0)
+            {
+                RandKey = ran.Next(0, count);
+                ansall = replay_str[RandKey];
+            }
+
+            return ansall;
+        }
+
+        public static long qq_get(string msg)
+        {
+            long group = 1;
+            dircheck(group);
+            XElement root = XElement.Load(path + group + ".xml");
+            string ansall = "";
+            long ansqq = 0;
+            foreach (XElement mm in root.Elements("msginfo"))
+            {
+                if (msg == mm.Element("ans").Value)
+                {
+                    ansall = mm.Element("msg").Value;
+                    break;
+                }
+            }
+
+            if (ansall != "")
+            {
+                ansqq = long.Parse(ansall);
+            }
+
+            return ansqq;
+        }
+
+        public static long qq_get_unregister(string msg)
+        {
+            long group = 5;
+            dircheck(group);
+            XElement root = XElement.Load(path + group + ".xml");
+            string ansall = "";
+            long ansqq = 0;
+            foreach (XElement mm in root.Elements("msginfo"))
+            {
+                if (msg == mm.Element("ans").Value)
+                {
+                    ansall = mm.Element("msg").Value;
+                    break;
+                }
+            }
+
+            if (ansall != "")
+            {
+                ansqq = long.Parse(ansall);
+            }
+
+            return ansqq;
+        }
+
+        public static string xml_get(long group, string msg)
+        {
+            dircheck(group);
+            XElement root = XElement.Load(path + group + ".xml");
+            string ansall = "";
+            foreach (XElement mm in root.Elements("msginfo"))
+            {
+                if (msg == mm.Element("msg").Value)
+                {
+                    ansall = mm.Element("ans").Value;
+                    break;
+                }
+            }
+
+            return ansall;
+        }
+
+        public static string xml_dic_get(string num)
+        {
+            XElement root = XElement.Load(path + "dic.xml");
+            string ans = "";
+            foreach (XElement mm in root.Elements("msginfo"))
+            {
+                if (num == mm.Element("sum").Value)
+                {
+                    ans = mm.Element("word").Value + "\r\n" + mm.Element("translate").Value;
+                    break;
+                }
+            }
+            return ans;
+        }
+
+        public static string list_get(long group, string msg)
+        {
+            dircheck(group);
+            XElement root = XElement.Load(path + group + ".xml");
+            int count = 0;
+            string ansall = "";
+            foreach (XElement mm in root.Elements("msginfo"))
+            {
+                if (msg == mm.Element("msg").Value)
+                {
+                    ansall = ansall + mm.Element("ans").Value + "\r\n";
+                    count++;
+                }
+            }
+            ansall = ansall + "一共有" + count.ToString() + "条回复";
+            return ansall;
+        }
+
+        public static void del(long group, string msg)
+        {
+            dircheck(group);
+            string gg = group.ToString();
+            XElement root = XElement.Load(path + group + ".xml");
+
+            var element = from ee in root.Elements()
+                          where (string)ee.Element("msg") == msg
+                          select ee;
+            if (element.Count() > 0)
+            {
+                //element.First().Remove();
+                element.Remove();
+            }
+            root.Save(path + group + ".xml");
+        }
+
+
+
+        public static void remove(long group, string msg, string ans)
+        {
+            dircheck(group);
+            string gg = group.ToString();
+            XElement root = XElement.Load(path + group + ".xml");
+
+            var element = from ee in root.Elements()
+                          where (string)ee.Element("msg") == msg && (string)ee.Element("ans") == ans
+                          select ee;
+            if (element.Count() > 0)
+            {
+                element.First().Remove();
+            }
+            root.Save(path + group + ".xml");
+        }
+
+
+        public static void insert(long group, string msg, string ans)
+        {
+            if (msg.IndexOf("\r\n") < 0 & msg != "")
+            {
+                dircheck(group);
+                XElement root = XElement.Load(path + group + ".xml");
+
+                XElement read = root.Element("msginfo");
+
+                read.AddBeforeSelf(new XElement("msginfo",
+                       //new XElement("group", group),
+                       new XElement("msg", msg),
+                       new XElement("ans", ans)
+                       ));
+
+                root.Save(path + group + ".xml");
+            }
+        }
+
+        public static void createxml(long group)
+        {
+            XElement root = new XElement("Categories",
+                new XElement("msginfo",
+                    //new XElement("group", 123),
+                    new XElement("msg", "初始问题"),
+                    new XElement("ans", "初始回答")
+                    )
+               );
+            root.Save(path + group + ".xml");
+        }
+
+        public static void dircheck(long group)
+        {
+            if (File.Exists(path + group + ".xml"))
+            {
+                //MessageBox.Show("存在文件");
+                //File.Delete(dddd);//删除该文件
+            }
+            else
+            {
+                //MessageBox.Show("不存在文件");
+                createxml(group);//创建该文件，如果路径文件夹不存在，则报错。
+            }
+        }
+
+        public static int AdminCheck(long fromQQ)
+        {
+            dircheck(123456);
+
+            XElement root = XElement.Load(path + "123456.xml");
+            int count = 0;
+            foreach (XElement mm in root.Elements("msginfo"))
+            {
+                if (mm.Element("ans").Value == fromQQ.ToString())
+                {
+                    count = 1;
+                }
+            }
+            return count;
+        }
+    }
+}
