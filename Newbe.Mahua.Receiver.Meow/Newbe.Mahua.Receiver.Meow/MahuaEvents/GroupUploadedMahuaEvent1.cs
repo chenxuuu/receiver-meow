fefaaf2d35c5043b3c4e3e378e7c3e13b28be5c3
chenxuuu <lolicon@papapoi.com>
2018-07-14 18:20:30 +0800

    上传文件提示

---
+++ Newbe.Mahua.Receiver.Meow/Newbe.Mahua.Receiver.Meow/MahuaEvents/GroupUploadedMahuaEvent1.cs
@@ -0,0 +1,27 @@
+﻿using Newbe.Mahua.MahuaEvents;
+using System;
+using Newbe.Mahua.Receiver.Meow.MahuaApis;
+
+namespace Newbe.Mahua.Receiver.Meow.MahuaEvents
+{
+    /// <summary>
+    /// 群文件上传事件
+    /// </summary>
+    public class GroupUploadedMahuaEvent1
+        : IGroupUploadedMahuaEvent
+    {
+        private readonly IMahuaApi _mahuaApi;
+
+        public GroupUploadedMahuaEvent1(
+            IMahuaApi mahuaApi)
+        {
+            _mahuaApi = mahuaApi;
+        }
+
+        public void ProcessGroupUploaded(GroupUploadedContext context)
+        {
+            _mahuaApi.SendGroupMessage(context.FromGroup,
+                "群员" + Tools.At(context.FromQq) + "上传了一个文件，大家快来看看这是什么吧！");
+        }
+    }
+}
