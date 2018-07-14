d89324ffbe477d8cc3eef9091ac62bc423eb87a8
chenxuuu <lolicon@papapoi.com>
2018-07-14 12:49:56 +0800

    qq宠物库

---
+++ Newbe.Mahua.Receiver.Meow/Newbe.Mahua.Receiver.Meow/MahuaApis/Tools.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Newbe.Mahua.Receiver.Meow.MahuaApis
+{
+    class Tools
+    {
+
+        /// <summary>  
+        /// GET 请求与获取结果（qq宠物专用，带cookie参数）  
+        /// </summary>  
+        public static string HttpGetPet(string Url, string postDataStr, string uin, string skey)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url + (postDataStr == "" ? "" : "?") + postDataStr);
+                request.Method = "GET";
+                request.ContentType = "text/html;charset=UTF-8";
+                request.Headers.Add("cookie", "uin=" + uin + "; skey=" + skey);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream myResponseStream = response.GetResponseStream();
+                StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.UTF8);
+
+                string retString = myStreamReader.ReadToEnd();
+                myStreamReader.Close();
+                myResponseStream.Close();
+
+                return retString;
+            }
+            catch { }
+            return "";
+        }
+
+        /// <summary>
+        /// 简化正则的操作
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="regstr"></param>
+        /// <returns></returns>
+        public static MatchCollection Reg_solve(string str, string regstr)
+        {
+            Regex reg = new Regex(regstr, RegexOptions.IgnoreCase);
+            return reg.Matches(str);
+        }
+
+        /// <summary>
+        /// 直接获取正则表达式的最后一组匹配值
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="regstr"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static string Reg_get(string str, string regstr, string name)
+        {
+            string result = "";
+            MatchCollection matchs = Reg_solve(str, regstr);
+            foreach (Match item in matchs)
+            {
+                if (item.Success)
+                {
+                    result = item.Groups[name].Value;
+                }
+            }
+            return result;
+        }
+    }
+}
