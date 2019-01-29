a49da710653cb873d63365fa22abc8a739ab6963
chenxuuu <lolicon@papapoi.com>
2019-01-29 14:39:22 +0800

    查快递可自定义快递公司

--- 全局词条的lua脚本/查快递.lua
+++ 全局词条的lua脚本/查快递.lua
@@ -1,18 +1,21 @@
-function search(expressNo)
-    if expressNo:len() == 0 then
-        expressNo = getData(fromqq,"search_for_kydi")
-    else
-        setData(fromqq,"search_for_kydi",expressNo)
+function search(expressNo,c)
+    local comCode = c
+    if not comCode then
+        if expressNo:len() == 0 then
+            expressNo = getData(fromqq,"search_for_kydi")
+        else
+            setData(fromqq,"search_for_kydi",expressNo)
+        end
+        if expressNo:len() == 0 then
+            return at(fromqq).."你没有历史查询记录哦"
+        end
+        local html = httpGet("https://www.kuaidi100.com/autonumber/autoComNum", "text="..expressNo)
+        if not html then
+            return at(fromqq).."数据加载失败"
+        end
+        local jsonC = JSON:decode(html)
+        comCode = jsonC.auto[1].comCode
     end
-    if expressNo:len() == 0 then
-        return at(fromqq).."你没有历史查询记录哦"
-    end
-    local html = httpGet("https://www.kuaidi100.com/autonumber/autoComNum", "text="..expressNo)
-    if not html then
-        return at(fromqq).."数据加载失败"
-    end
-    local jsonC = JSON:decode(html)
-    local comCode = jsonC.auto[1].comCode
     if not comCode then
         return at(fromqq).."没找到这个快递"
     end
@@ -30,5 +33,7 @@ function search(expressNo)
 end
 
 if message:find("查快递") == 1 then
-    print(search(message:gsub("查快递","")))
+    local data = message:gsub("查快递 *","")
+    local id = data:split(" ")
+    print(search(id[1],id[2]))
 end
