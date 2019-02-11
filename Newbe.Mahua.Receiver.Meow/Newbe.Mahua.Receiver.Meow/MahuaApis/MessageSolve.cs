dbcc62243ff7172cf2a8d35d6f9aae251f4f2ac4
chenxuuu <lolicon@papapoi.com>
2019-02-11 10:55:41 +0800

    帮助返回内容改为脚本

--- Newbe.Mahua.Receiver.Meow/Newbe.Mahua.Receiver.Meow/MahuaApis/MessageSolve.cs
+++ Newbe.Mahua.Receiver.Meow/Newbe.Mahua.Receiver.Meow/MahuaApis/MessageSolve.cs
@@ -7,6 +7,7 @@ using Newbe.Mahua.MahuaEvents;
 using Newbe.Mahua.Receiver.Meow.MahuaApis;
 using Newbe.Mahua.Receiver.Meow;
 using System.Web;
+using System.IO;
 
 namespace Newbe.Mahua.Receiver.Meow.MahuaApis
 {
@@ -54,23 +55,9 @@ namespace Newbe.Mahua.Receiver.Meow.MahuaApis
             }
             else if (msg.ToUpper() == "HELP" || msg == "帮助" || msg == "菜单")
             {
-                result += @"命令帮助[CQ:emoji,id=128172]
-[CQ:emoji,id=128227]！add 关键词：回答
-[CQ:emoji,id=128465]！del 关键词：回答
-[CQ:emoji,id=128221]！list 关键词
-[CQ:emoji,id=128465]！delall 关键词
-[CQ:emoji,id=128683]！ban 禁止响应的词
-[CQ:emoji,id=9989]！unban 恢复响应的词
-[CQ:emoji,id=127806]坷垃金曲+数字序号（最大71）
-[CQ:emoji,id=128077]点赞
-[CQ:emoji,id=127881]今日运势
-[CQ:emoji,id=128667]查快递 加 单号
-[CQ:emoji,id=128168]空气质量
-[CQ:emoji,id=127932]点歌 加 网易云id或歌名
-[CQ:emoji,id=128252]复读 加 百分比
-[CQ:emoji,id=128444]查动画 加 没裁剪过的视频截图
-[CQ:emoji,id=128295]！lua 查看lua工具使用说明
-[CQ:emoji,id=128483]自己运行/反馈：https://github.com/chenxuuu/receiver-meow";
+                result += Tools.RunLua(File.ReadAllText(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "全局词条的lua脚本/help.lua"),
+                            "fromqq = \"" + fromqq + "\"\r\n" +
+                            "fromgroup = \"" + fromgroup + "\"");
             }
             else if(msg.ToUpper()=="！LUA")
             {
