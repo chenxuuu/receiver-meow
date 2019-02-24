651f0cee16aea32e6e2d90a388eb42960ad6f59f
chenxuuu <lolicon@papapoi.com>
2019-02-24 22:00:20 +0800

    重写编写

---
+++ com.papapoi.ReceiverMeow/Native.Csharp.Tool/NativeConvert.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Native.Csharp.Tool.Core;
+
+namespace Native.Csharp.Tool
+{
+	/// <summary>
+	/// Native 用于数据转换的工具类
+	/// </summary>
+	public static class NativeConvert
+	{
+		/// <summary>
+		/// 获取10或13位时间戳的 System.DateTime 表示形式
+		/// </summary>
+		/// <param name="timeStamp">10 or 13 位时间戳</param>
+		/// <returns></returns>
+		public static DateTime FotmatUnixTime (string timeStamp)
+		{
+			DateTime dtStart = TimeZone.CurrentTimeZone.ToLocalTime (new DateTime (1970, 1, 1));
+			long lTime;
+			if (timeStamp.Length.Equals (10))//判断是10位
+			{
+				lTime = long.Parse (timeStamp + "0000000");
+			}
+			else
+			{
+				lTime = long.Parse (timeStamp + "0000");//13位
+			}
+			TimeSpan toNow = new TimeSpan (lTime);
+			DateTime daTime = dtStart.Add (toNow);
+			return daTime;
+		}
+
+		/// <summary>
+		/// 获取指定 IntPtr 实例中的字符串
+		/// </summary>
+		/// <param name="strPtr">字符串的 IntPtr 对象</param>
+		/// <param name="encoding">目标编码格式</param>
+		/// <returns></returns>
+		public static string ToPtrString (IntPtr strPtr, Encoding encoding = null)
+		{
+			if (encoding == null)
+			{
+				encoding = Encoding.Default;
+			}
+
+			int len = Kernel32.LstrlenA (strPtr);   //获取指针中数据的长度
+			if (len == 0)
+			{
+				return string.Empty;
+			}
+
+			byte[] buffer = new byte[len];
+			Marshal.Copy (strPtr, buffer, 0, len);
+			return encoding.GetString (buffer);
+		}
+
+		/// <summary>
+		/// 获取字符串的 IntPtr 实例对象
+		/// </summary>
+		/// <param name="value">将转换的字符串</param>
+		/// <param name="encoding">目标编码格式</param>
+		/// <returns></returns>
+		public static IntPtr ToStringPtr (string value, Encoding encoding = null)
+		{
+			if (encoding == null)
+			{
+				encoding = Encoding.Default;
+			}
+			byte[] buffer = encoding.GetBytes (value);
+			GCHandle hobj = GCHandle.Alloc (buffer, GCHandleType.Pinned);
+			return hobj.AddrOfPinnedObject ();
+		}
+	}
+}
---
+++ com.papapoi.ReceiverMeow/Native.Csharp/App/LuaEnv/Tools.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Native.Csharp.App.LuaEnv
+{
+    class Tools
+    {
+        /// <summary>
+        /// 简化正则的操作
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="regstr"></param>
+        /// <returns></returns>
+        public static MatchCollection Reg_solve(string str, string regstr)
+        {
+            Regex reg = new Regex(regstr, RegexOptions.IgnoreCase);
+            return reg.Matches(str);
+        }
+
+        /// <summary>
+        /// 直接获取正则表达式的最后一组匹配值
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="regstr"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static string Reg_get(string str, string regstr, string name)
+        {
+            string result = "";
+            MatchCollection matchs = Reg_solve(str, regstr);
+            foreach (Match item in matchs)
+            {
+                if (item.Success)
+                {
+                    result = item.Groups[name].Value;
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 直接获取正则表达式的所有匹配值
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="regstr"></param>
+        /// <param name="name"></param>
+        /// <param name="name">连接符</param>
+        /// <returns></returns>
+        public static string Reg_get_all(string str, string regstr, string name, string connect)
+        {
+            string result = "";
+            MatchCollection matchs = Reg_solve(str, regstr);
+            foreach (Match item in matchs)
+            {
+                if (item.Success)
+                {
+                    result += connect + item.Groups[name].Value;
+                }
+            }
+            if (result != "")
+                return result.Substring(connect.Length);
+            else
+                return result;
+        }
+
+        /// <summary>
+        /// Save a binary file to disk.
+        /// </summary>
+        /// <param name="response">The response used to save the file</param>
+        public static bool SaveBinaryFile(WebResponse response, string FileName)
+        {
+            bool Value = true;
+            byte[] buffer = new byte[1024];
+
+            try
+            {
+                if (File.Exists(FileName))
+                    File.Delete(FileName);
+                Stream outStream = System.IO.File.Create(FileName);
+                Stream inStream = response.GetResponseStream();
+                //inStream.ContentLength
+                int l;
+                do
+                {
+                    l = inStream.Read(buffer, 0, buffer.Length);
+                    if (l > 0)
+                        outStream.Write(buffer, 0, l);
+                }
+                while (l > 0);
+
+                outStream.Close();
+                inStream.Close();
+            }
+            catch
+            {
+                Value = false;
+            }
+
+            return Value;
+        }
+
+        ///<summary>
+        ///生成随机字符串
+        ///</summary>
+        ///<param name="length">目标字符串的长度</param>
+        ///<param name="useNum">是否包含数字，1=包含，默认为包含</param>
+        ///<param name="useLow">是否包含小写字母，1=包含，默认为包含</param>
+        ///<param name="useUpp">是否包含大写字母，1=包含，默认为包含</param>
+        ///<param name="useSpe">是否包含特殊字符，1=包含，默认为不包含</param>
+        ///<param name="custom">要包含的自定义字符，直接输入要包含的字符列表</param>
+        ///<returns>指定长度的随机字符串</returns>
+        public static string GetRandomString(int length, bool useNum, bool useLow, bool useUpp, bool useSpe, string custom)
+        {
+            byte[] b = new byte[4];
+            new System.Security.Cryptography.RNGCryptoServiceProvider().GetBytes(b);
+            Random r = new Random(BitConverter.ToInt32(b, 0));
+            string s = null, str = custom;
+            if (useNum == true) { str += "0123456789"; }
+            if (useLow == true) { str += "abcdefghijklmnopqrstuvwxyz"; }
+            if (useUpp == true) { str += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; }
+            if (useSpe == true) { str += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"; }
+            for (int i = 0; i < length; i++)
+            {
+                s += str.Substring(r.Next(0, str.Length - 1), 1);
+            }
+            return s;
+        }
+
+
+        ///  <summary>
+        /// 获取指定驱动器的剩余空间总大小(单位为MB)
+        ///  </summary>
+        ///  <param name="str_HardDiskName">只需输入代表驱动器的字母即可 </param>
+        ///  <returns> </returns>
+        public static long GetHardDiskFreeSpace(string str_HardDiskName)
+        {
+            long freeSpace = new long();
+            str_HardDiskName = str_HardDiskName + ":\\";
+            System.IO.DriveInfo[] drives = System.IO.DriveInfo.GetDrives();
+            foreach (System.IO.DriveInfo drive in drives)
+            {
+                if (drive.Name == str_HardDiskName)
+                {
+                    freeSpace = drive.TotalFreeSpace / (1024 * 1024);
+                }
+            }
+            return freeSpace;
+        }
+
+
+    }
+}
