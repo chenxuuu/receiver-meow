0c1d828f20915e60e8365b92bb8d177d010e544d
chenxuuu <lolicon@papapoi.com>
2019-03-01 22:24:40 +0800

    lua测试文件

--- appdata/lua/require/head.lua
+++ appdata/lua/require/head.lua
@@ -1,4 +1,14 @@
 --提前运行的脚本
 --用于提前声明某些要用到的函数
 
+--加强随机数随机性
+math.randomseed(tostring(os.time()):reverse():sub(1, 6))
 
+--防止跑死循环，超时15秒自动结束
+local start = os.time()
+function trace (event, line)
+    if os.time() - start >=15 then
+        error("运行超时")
+    end
+end
+debug.sethook(trace, "l")
