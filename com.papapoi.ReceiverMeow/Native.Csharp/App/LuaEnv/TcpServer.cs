adc492c2dda7b5c198a241b148d387267f9d8da2
chenxuuu <lolicon@papapoi.com>
2019-04-01 23:36:35 +0800

    tcp发送加上队列延时机制

--- com.papapoi.ReceiverMeow/Native.Csharp/App/LuaEnv/TcpServer.cs
+++ com.papapoi.ReceiverMeow/Native.Csharp/App/LuaEnv/TcpServer.cs
@@ -1,5 +1,6 @@
 ﻿using SimpleTCP;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,11 @@ namespace Native.Csharp.App.LuaEnv
 {
     class TcpServer
     {
+        //需要发送的包列表
+        private static ArrayList toSend = new ArrayList();
+        //每个包发送间隔时间（可以自己改）
+        private static int packTime = 1000;
+
         private static SimpleTcpServer server = new SimpleTcpServer();
         public static void Start()
         {
@@ -19,7 +25,6 @@ namespace Native.Csharp.App.LuaEnv
                 Common.CqApi.AddLoger(Sdk.Cqp.Enum.LogerLevel.Info, "tcp server",
                     "tcp server started!");
                 server.DataReceived += (sender, msg) => {
-                    //msg.Reply("Content-Type: text/plain\n\nHello from my web server!");
                     LuaEnv.RunLua(
                         $"message=[[{msg.MessageString.Replace("]", "] ")}]] ",
                         "envent/ReceiveTcp.lua");
@@ -30,13 +35,30 @@ namespace Native.Csharp.App.LuaEnv
                 Common.CqApi.AddLoger(Sdk.Cqp.Enum.LogerLevel.Error, "tcp server", 
                     "tcp server start failed!\r\n"+e.ToString());
             }
+
+            //消息发送队列
+            Task.Run(() =>
+            {
+                while(true)
+                {
+                    while (toSend.Count > 0)
+                    {
+                        string temp = toSend[0].ToString();//取出第一个数据
+                        toSend.RemoveAt(0);
+                        server.Broadcast(temp);
+                        Task.Delay(packTime).Wait();
+                    }
+                    Task.Delay(200).Wait();//等等，防止卡死
+                }
+            });
         }
 
         public static void Send(string msg)
         {
             try
             {
-                server.Broadcast(msg);
+                //server.Broadcast(msg);
+                toSend.Add(msg);
             }
             catch (Exception e)
             {
