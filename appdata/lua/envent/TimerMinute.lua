98ba249ec310daf5f7403d481140c34bfb2bf7d6
chenxuuu <lolicon@papapoi.com>
2019-04-08 21:53:53 +0800

    更正时间判断代码

--- appdata/lua/envent/TimerMinute.lua
+++ appdata/lua/envent/TimerMinute.lua
@@ -6,8 +6,7 @@
 详细请参考readme
 ]]
 
-hour = math.floor(os.time()/3600) % 24
-minute = math.floor(os.time()/60) % 60
+local time = os.date("*t")
 
 lowSpace = 10 --剩余空间多少G
 
@@ -15,16 +14,20 @@ function getSpaceOk()
     return apiGetHardDiskFreeSpace("D") > 1024 * lowSpace
 end
 
-if minute == 0 and not getSpaceOk() then
+if time.min == 0 and not getSpaceOk() then
     cqSendGroupMessage(567145439,
     cqCode_At(961726194).."你的小垃圾服务器空间只有"..tostring(apiGetHardDiskFreeSpace("D")).."M空间了知道吗？快去清理")
 end
 
-if minute == 59 and hour == 2 and getSpaceOk() then
+if time.min == 59 and time.hour == 2 and getSpaceOk() then
     cqSendGroupMessage(241464054,"一分钟后，将自动进行服务器例行重启与资源世界回档，请注意自己身上的物品")
     apiTcpSend("一分钟后，将自动进行服务器例行重启与资源世界回档，请注意自己身上的物品")
 end
 
-if minute == 0 and hour == 3 and getSpaceOk() then
+if time.min == 0 and time.hour == 3 and getSpaceOk() then
     apiTcpSend("cmdstop")
+end
+
+if time.min == 0 and time.hour == 5 then
+    apiTcpSend("cmdworld create mine")
 end
