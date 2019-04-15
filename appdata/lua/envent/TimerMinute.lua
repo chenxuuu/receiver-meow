d3dcde8f7ccf30481d6e17d779793ebf2e753ad6
chenxuuu <lolicon@papapoi.com>
2019-04-15 16:09:51 +0800

    GitHub更新检查改为10分钟一次，限定官方机器人才有这功能 更改页面链接改为短网址

--- appdata/lua/envent/TimerMinute.lua
+++ appdata/lua/envent/TimerMinute.lua
@@ -6,6 +6,8 @@
 详细请参考readme
 ]]
 
+if cqGetLoginQQ() ~= 751323264 then return end--仅限官方群里的机器人号用这个功能
+
 local time = os.date("*t")
 
 lowSpace = 10 --剩余空间多少G
@@ -33,25 +35,32 @@ if time.min == 0 and time.hour == 5 then
 end
 
 --检查GitHub项目是否有更新
-local githubRss = apiHttpGet("https://github.com/chenxuuu/receiver-meow/commits/master.atom")
-if githubRss or githubRss ~= "" then--获取成功的话
-    local xml2lua = require("xml2lua")
-    --Uses a handler that converts the XML to a Lua table
-    local handler = require("xmlhandler.tree")
-    local parser = xml2lua.parser(handler)
-    parser:parse(githubRss)
-    local lastUpdate = handler.root.feed.updated
-    if lastUpdate and lastUpdate ~= apiXmlGet("settings","githubLastUpdate") then
-        apiXmlSet("settings","githubLastUpdate",lastUpdate)
-        for i,j in pairs(handler.root.feed.entry) do
-            local toSend = "接待喵lua插件在GitHub上有更新啦\r\n"..
-            "更新时间："..(lastUpdate):gsub("T"," "):gsub("Z"," ").."\r\n"..
-            "提交内容："..j.title.."\r\n"..
-            "查看变动代码："..j.link._attr.href
-            cqSendGroupMessage(931546484, toSend)
-            break
+if time.min % 10 == 0 then--十分钟检查一次
+    local githubRss = apiHttpGet("https://github.com/chenxuuu/receiver-meow/commits/master.atom")
+    if githubRss or githubRss ~= "" then--获取成功的话
+        local xml2lua = require("xml2lua")
+        --Uses a handler that converts the XML to a Lua table
+        local handler = require("xmlhandler.tree")
+        local parser = xml2lua.parser(handler)
+        parser:parse(githubRss)
+        local lastUpdate = handler.root.feed.updated
+        if lastUpdate and lastUpdate == apiXmlGet("settings","githubLastUpdate") then
+            apiXmlSet("settings","githubLastUpdate",lastUpdate)
+            for i,j in pairs(handler.root.feed.entry) do
+                --缩短网址
+                local shortUrl = apiHttpPost("https://git.io/create","url="..j.link._attr.href:urlEncode())
+                shortUrl = (not shortUrl or shortUrl == "") and j.link._attr.href or "https://biu.papapoi.com/"..shortUrl
+
+                --返回结果
+                local toSend = "接待喵lua插件在GitHub上有更新啦\r\n"..
+                "更新时间："..(lastUpdate):gsub("T"," "):gsub("Z"," ").."\r\n"..
+                "提交内容："..j.title.."\r\n"..
+                "查看变动代码："..shortUrl
+                print(toSend)
+                --cqSendGroupMessage(931546484, toSend)
+                break
+            end
         end
     end
 end
-
 
