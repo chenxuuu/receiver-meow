2ba9c32429e6f84d6060ec556e2299958015973f
chenxuuu <lolicon@papapoi.com>
2019-04-26 00:21:24 +0800

    b站视频解析接口改为官方接口

--- appdata/lua/require/app/av.lua
+++ appdata/lua/require/app/av.lua
@@ -1,18 +1,15 @@
 --解析b站av号
 function getInfo(av)
     av = av:match("av(%d+)")
-    local html = apiHttpGet("https://www.jijidown.com/api/v1/video/get_info?id="..av)
+    local html = apiHttpGet("http://api.bilibili.com/x/web-interface/view?aid="..av)
     if not html then return "查找失败" end
     local j,r,e = jsonDecode(html)
-    if j.title == "正在加载数据..." then
-        return getInfo(av)
-    end
-    if not r or j.upid == -1 then return "数据解析失败啦" end
-    image = j.img and image("http:"..j.img)
+    if j.code ~= 0 then return "数据解析失败啦" end
+    image = j.data.pic and image(j.data.pic)
     return (image and image.."\r\n" or "")..
-    "av"..av..",标题："..j.title..
-    "\r\n"..j.desc:gsub("<br/>","\r\n")..
-    "\r\n分区："..(j.sort and j.sort..(j.subsort and "->"..j.subsort or "") or "")
+    "av"..av..",标题："..j.data.title..
+    "\r\n"..j.data.desc:gsub("<br/>","\r\n")..
+    "\r\n分区："..(j.data.tname and j.data.tname or "")
 end
 
 return getInfo
