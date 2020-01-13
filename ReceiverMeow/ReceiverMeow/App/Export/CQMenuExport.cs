8f0333503b354bd5e2e003d9503b3dc5c7fd1674
chenxuuu <lolicon@papapoi.com>
2020-01-13 23:26:48 +0800

    整理目录

---
+++ ReceiverMeow/ReceiverMeow/App/Export/CQMenuExport.cs
@@ -0,0 +1,51 @@
+/*
+ * 此文件由T4引擎自动生成, 请勿修改此文件中的代码!
+ */
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Native.Csharp.Sdk.Cqp;
+using Native.Csharp.App.Common;
+using Native.Csharp.Sdk.Cqp.EventArgs;
+using Native.Csharp.Sdk.Cqp.Interface;
+using Unity;
+
+namespace Native.Csharp.App.Export
+{
+	/// <summary>	
+	/// 表示酷Q菜单导出的类	
+	/// </summary>	
+	public class CQMenuExport	
+	{	
+		#region --字段--	
+		private static CQApi api = null;	
+		private static CQLog log = null;	
+		#endregion	
+		
+		#region --构造函数--	
+		/// <summary>	
+		/// 由托管环境初始化的 <see cref="CQMenuExport"/> 的新实例	
+		/// </summary>	
+		static CQMenuExport ()	
+		{	
+			api = AppInfo.UnityContainer.Resolve<CQApi> (AppInfo.Id);	
+			log = AppInfo.UnityContainer.Resolve<CQLog> (AppInfo.Id);	
+			
+			// 调用方法进行实例化	
+			ResolveBackcall ();	
+		}	
+		#endregion	
+		
+		#region --私有方法--	
+		/// <summary>	
+		/// 读取容器中的注册项, 进行事件分发	
+		/// </summary>	
+		private static void ResolveBackcall ()	
+		{	
+		}	
+		#endregion	
+		
+		#region --导出方法--	
+		#endregion	
+	}	
+}
