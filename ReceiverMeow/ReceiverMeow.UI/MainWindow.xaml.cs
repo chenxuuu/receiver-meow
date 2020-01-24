a15872e79a19c56337124091d4e9a0afb2f192cf
chenxuuu <lolicon@papapoi.com>
2020-01-24 21:25:42 +0800

    设置窗口基本完成

--- ReceiverMeow/ReceiverMeow.UI/MainWindow.xaml.cs
+++ ReceiverMeow/ReceiverMeow.UI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using Native.Csharp.Sdk.Cqp.EventArgs;
+using Native.Csharp.Sdk.Cqp.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +20,37 @@ namespace ReceiverMeow.UI
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IMenuCall
     {
+        private MainWindow window = null;
+
+        public void MenuCall(object sender, CQMenuCallEventArgs e)
+        {
+            if (window == null)
+            {
+                window = new MainWindow();
+                window.Closing += (ss, ee) =>
+                {
+                    window = null;
+                };
+                window.Show();
+            }
+            else
+            {
+                window.Activate();
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = Global.Settings;
+        }
+
+
     }
 }
