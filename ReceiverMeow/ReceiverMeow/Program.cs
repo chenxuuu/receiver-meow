5f848e7afa8b4e32d2d5c1e5a281fabc8187b666
chenxuuu <lolicon@papapoi.com>
2020-12-13 15:18:14 +0800

    切换到.net core，待改

---
+++ ReceiverMeow/ReceiverMeow/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using WebSocketSharp;
+
+namespace ReceiverMeow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var lua = new NLua.Lua();
+            lua.DoString("print(1213122)");
+
+            using (var ws = new WebSocket("ws://tcplab.openluat.com:12421/"))
+            {
+                ws.OnMessage += (sender, e) =>
+                    Console.WriteLine("Laputa says: " + e.Data);
+
+                ws.Connect();
+                ws.Send("{\"function\":0}");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
