0a0cf6fec2ed0e882ab4cd5b24ee08425c2900a5
chenxuuu <lolicon@papapoi.com>
2020-12-18 12:49:28 +0800

    更正http端口设置没生效的问题

--- ReceiverMeow/ReceiverMeow/GoHttp/Http.cs
+++ ReceiverMeow/ReceiverMeow/GoHttp/Http.cs
@@ -33,11 +33,11 @@ namespace ReceiverMeow.GoHttp
         {
             Log.Debug(module, $"发送{json}到{path}");
             var client = new RestClient();
-            client.BaseUrl = new Uri($"http://127.0.0.1:5700/{path}");
+            client.BaseUrl = new Uri($"http://{host}:{port}/{path}");
             var request = new RestRequest(RestSharp.Method.POST);
             request.AddParameter("application/json", json, ParameterType.RequestBody);
             var response = client.Execute(request);
-            
+
             if(response.StatusCode != System.Net.HttpStatusCode.OK)
                 Log.Warn(module, $"HTTP返回状态码：{(int)response.StatusCode}");
             return Encoding.UTF8.GetString(response.RawBytes);
